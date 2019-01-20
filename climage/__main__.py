95600f554fcce57fab3bae40f243c5449a0ddcfd
Patrick Nappa <patricknappa@gmail.com>
2019-01-20 21:12:48 +1100

    added 8 and 16 color modes, as well as selectable system palettes

--- climage/__main__.py
+++ climage/__main__.py
@@ -3,19 +3,33 @@
 
 from . import __version__
 from .climage import _toAnsi
+from .climage import _color_types
 
 from PIL import Image
 import argparse
 
-def convert(filename, is_unicode=False, is_truecolor=False, width=80):
+def _get_color_type(is_truecolor, is_256color, is_16color, is_8color):
+    assert int(bool(is_8color)) + int(bool(is_16color)) + int(bool(is_256color)) + int(bool(is_truecolor)) == 1 and "only pick one colormode"
+
+    if is_truecolor:
+        return _color_types.truecolor
+    if is_256color:
+        return _color_types.color256
+    if is_16color:
+        return _color_types.color16
+    if is_8color:
+        return _color_types.color8
+
+def convert(filename, is_unicode=False, is_truecolor=False, is_256color=True, is_16color=False, is_8color=False, width=80, palette="xterm"):
     # open the img, but convert to rgb because this fails if grayscale 
     # (assumes pixels are at least triplets)
     im = Image.open(filename).convert('RGB')
-    return _toAnsi(im, oWidth=width, is_unicode=is_unicode, is_truecolor=is_truecolor)
+    ctype = _get_color_type(is_truecolor=is_truecolor, is_256color=is_256color, is_16color=is_16color, is_8color=is_8color)
+    return _toAnsi(im, oWidth=width, is_unicode=is_unicode, color_type=ctype, palette=palette)
 
-def to_file(infile, outfile, is_unicode=False, is_truecolor=False, width=80):
+def to_file(infile, outfile, is_unicode=False, is_truecolor=False, is_256color=True, is_16color=False, is_8color=False, width=80, palette="xterm"):
     with open(outfile, 'w') as ofile:
-        ansi_str = convert(infile, is_unicode=is_unicode, is_truecolor=is_truecolor, width=width)
+        ansi_str = convert(infile, is_unicode=is_unicode, is_truecolor=is_truecolor, is_256color=is_256color, is_16color=is_16color, is_8color=is_8color, width=width, palette=palette)
         ofile.write(convert)
 
 def main():
@@ -35,7 +49,11 @@ def main():
 
     color_type_group = arg_parser.add_mutually_exclusive_group()
     color_type_group.add_argument('--truecolor', '-t', help="Utilise 16 million colors to encode output, results in more accurate output. Warning: RGB color is not supported by all terminals.", action="store_true")
-    color_type_group.add_argument('--256', '-m', help="Only use 256 colors to encode output (default).", action="store_true", default=True)
+    color_type_group.add_argument('--256color', '-m', dest='color256', help="Only use 256 colors to encode output (default).", action="store_true")
+    color_type_group.add_argument('--16color', '-s', dest='color16', help="Only use 16 colors to encode output.", action="store_true")
+    color_type_group.add_argument('--8color', '-b', dest='color8', help="Only use 8 colors to encode output.", action="store_true")
+
+    arg_parser.add_argument('--palette', '-p', choices=["xterm", "rxvt", "solarized", "tango", "linuxconsole"], default="xterm", help="Choose a system color palette - only applies to 16 or 256 color modes. This is especially helpful for terminal themes that drastically change the appearance of default collors, achieving more accurate colors on those terminals.")
 
     arg_parser.add_argument('-w', '--cols', default=80, metavar='cols', help='Set the number of columns output should contain (default 80).', type=int)
 
@@ -53,21 +71,21 @@ def main():
         is_unicode = True
 
     # what mode of color should be used
-    is_truecolor = False
-    if args.truecolor:
-        is_truecolor = True
+    is_truecolor = args.truecolor
+    is_256color = args.color256
+    is_16color = args.color16
+    is_8color = args.color8
+
+    if not is_truecolor and not is_256color and not is_16color and not is_8color:
+        is_256color = True
 
     # width of the output image
     num_cols = args.cols
 
+    palette = args.palette
+
     # print to file, or stdout?
     if outfile != '-':
-        to_file(infile, outfile, is_unicode=is_unicode, is_truecolor=is_truecolor, width=num_cols)
+        to_file(infile, outfile, is_unicode=is_unicode, is_truecolor=is_truecolor, is_256color=is_256color, is_16color=is_16color, is_8color=is_8color, width=num_cols, palette=palette)
     else:
-        print(convert(infile, is_unicode=is_unicode, is_truecolor=is_truecolor, width=num_cols))
-
-
-# TODO: remove this
-if __name__ == "__main__":
-    main()
-
+        print(convert(infile, is_unicode=is_unicode, is_truecolor=is_truecolor, is_256color=is_256color, is_16color=is_16color, is_8color=is_8color, width=num_cols, palette=palette))
--- climage/climage.py
+++ climage/climage.py
@@ -9,7 +9,28 @@ def _best(candidates, source):
                                          abs(x[1] - source[1]) + 
                                          abs(x[2] - source[2]))
 
-def _rgb_to_256(r, g, b):
+class _color_types:
+    truecolor = 0
+    color256 = 1
+    color16 = 2
+    color8 = 3
+
+def _get_system_colors(palette):
+    # see extras/colorextract.py for details on getting these values
+    if palette == "xterm":
+        return [[0, 0, 0, 0], [205, 0, 0, 1], [0, 205, 0, 2], [205, 205, 0, 3], [0, 0, 238, 4], [205, 0, 205, 5], [0, 205, 205, 6], [229, 229, 229, 7], [127, 127, 127, 8], [255, 0, 0, 9], [0, 255, 0, 10], [255, 255, 0, 11], [92, 92, 255, 12], [255, 0, 255, 13], [0, 255, 255, 14], [255, 255, 255, 15]]
+    elif palette == "linuxconsole":
+        return [[0, 0, 0, 0], [170, 0, 0, 1], [0, 170, 0, 2], [170, 85, 0, 3], [0, 0, 170, 4], [170, 0, 170, 5], [0, 170, 170, 6], [170, 170, 170, 7], [85, 85, 85, 8], [255, 85, 85, 9], [85, 255, 85, 10], [255, 255, 85, 11], [85, 85, 255, 12], [255, 85, 255, 13], [85, 255, 255, 14], [255, 255, 255, 15]]
+    elif palette == "solarized":
+        return [[7, 54, 66, 0], [220, 50, 47, 1], [133, 153, 0, 2], [181, 137, 0, 3], [38, 139, 210, 4], [211, 54, 130, 5], [42, 161, 152, 6], [238, 232, 213, 7], [0, 43, 54, 8], [203, 75, 22, 9], [88, 110, 117, 10], [101, 123, 131, 11], [131, 148, 150, 12], [108, 113, 196, 13], [147, 161, 161, 14], [253, 246, 227, 15]]
+    elif palette == "rxvt":
+        return [[0, 0, 0, 0], [205, 0, 0, 1], [0, 205, 0, 2], [205, 205, 0, 3], [0, 0, 205, 4], [205, 0, 205, 5], [0, 205, 205, 6], [250, 235, 215, 7], [64, 64, 64, 8], [255, 0, 0, 9], [0, 255, 0, 10], [255, 255, 0, 11], [0, 0, 255, 12], [255, 0, 255, 13], [0, 255, 255, 14], [255, 255, 255, 15]]
+    elif palette == "tango":
+        return [[0, 0, 0, 0], [204, 0, 0, 1], [78, 154, 6, 2], [196, 160, 0, 3], [52, 101, 164, 4], [117, 80, 123, 5], [6, 152, 154, 6], [211, 215, 207, 7], [85, 87, 83, 8], [239, 41, 41, 9], [138, 226, 52, 10], [252, 233, 79, 11], [114, 159, 207, 12], [173, 127, 168, 13], [52, 226, 226, 14], [238, 238, 236, 15]]
+    else:
+        raise ValueError("invalid palette {}".format(palette))
+
+def _rgb_to_256(r, g, b, palette):
     # TODO: i reckon I can replace this with a fast voronoi version
     # i.e. generate voronoi boundaries offline, then find which
     # bound the point is in.
@@ -17,23 +38,6 @@ def _rgb_to_256(r, g, b):
     def gen_colours():
         # credit: https://github.com/dom111/image-to-ansi/
         # I actually don't know how he came up with these colours
-        colour_mapping.append([0, 0, 0, 0])
-        colour_mapping.append([128, 0, 0, 1])
-        colour_mapping.append([0, 128, 0, 2])
-        colour_mapping.append([128, 128, 0, 3])
-        colour_mapping.append([0, 0, 128, 4])
-        colour_mapping.append([128, 0, 128, 5])
-        colour_mapping.append([0, 128, 128, 6])
-        colour_mapping.append([192, 192, 192, 7])
-        colour_mapping.append([128, 128, 128, 8])
-        colour_mapping.append([255, 0, 0, 9])
-        colour_mapping.append([0, 255, 0, 10])
-        colour_mapping.append([255, 255, 0, 11])
-        colour_mapping.append([0, 0, 255, 12])
-        colour_mapping.append([255, 0, 255, 13])
-        colour_mapping.append([0, 255, 255, 14])
-        colour_mapping.append([255, 255, 255, 15])
-
         for r1 in [0, 95, 135, 175, 215, 255]:
             for g1 in [0, 95, 135, 175, 215, 255]:
                 for b1 in [0, 95, 135, 175, 215, 255]:
@@ -50,42 +54,67 @@ def _rgb_to_256(r, g, b):
 
     r, g, b = map(int, (r, g, b))
 
-    return _best(colour_mapping, [r, g, b])[3]
+    return _best(colour_mapping + _get_system_colors(palette), [r, g, b])[3]
 
 # convert a rgb color to the closest 16 color number
-def _rgb_to_16(r, g, b, palette="xterm"):
-    # see extras/colorextract.py for details on getting these values
-    color16_mapping = None
-    if palette == "xterm":
-        color16_mapping = [[0, 0, 0, 0], [205, 0, 0, 1], [0, 205, 0, 2], [205, 205, 0, 3], [0, 0, 238, 4], [205, 0, 205, 5], [0, 205, 205, 6], [229, 229, 229, 7], [127, 127, 127, 8], [255, 0, 0, 9], [0, 255, 0, 10], [255, 255, 0, 11], [92, 92, 255, 12], [255, 0, 255, 13], [0, 255, 255, 14], [255, 255, 255, 15]]
-    elif palette == "linuxconsole":
-        color16_mapping = [[0, 0, 0, 0], [170, 0, 0, 1], [0, 170, 0, 2], [170, 85, 0, 3], [0, 0, 170, 4], [170, 0, 170, 5], [0, 170, 170, 6], [170, 170, 170, 7], [85, 85, 85, 8], [255, 85, 85, 9], [85, 255, 85, 10], [255, 255, 85, 11], [85, 85, 255, 12], [255, 85, 255, 13], [85, 255, 255, 14], [255, 255, 255, 15]]
-    elif palette == "solarized":
-        color16_mapping = [[7, 54, 66, 0], [220, 50, 47, 1], [133, 153, 0, 2], [181, 137, 0, 3], [38, 139, 210, 4], [211, 54, 130, 5], [42, 161, 152, 6], [238, 232, 213, 7], [0, 43, 54, 8], [203, 75, 22, 9], [88, 110, 117, 10], [101, 123, 131, 11], [131, 148, 150, 12], [108, 113, 196, 13], [147, 161, 161, 14], [253, 246, 227, 15]]
-    elif palette == "rxvt":
-        color16_mapping = [[0, 0, 0, 0], [205, 0, 0, 1], [0, 205, 0, 2], [205, 205, 0, 3], [0, 0, 205, 4], [205, 0, 205, 5], [0, 205, 205, 6], [250, 235, 215, 7], [64, 64, 64, 8], [255, 0, 0, 9], [0, 255, 0, 10], [255, 255, 0, 11], [0, 0, 255, 12], [255, 0, 255, 13], [0, 255, 255, 14], [255, 255, 255, 15]]
-    elif palette == "tango":
-        color16_mapping = [[0, 0, 0, 0], [204, 0, 0, 1], [78, 154, 6, 2], [196, 160, 0, 3], [52, 101, 164, 4], [117, 80, 123, 5], [6, 152, 154, 6], [211, 215, 207, 7], [85, 87, 83, 8], [239, 41, 41, 9], [138, 226, 52, 10], [252, 233, 79, 11], [114, 159, 207, 12], [173, 127, 168, 13], [52, 226, 226, 14], [238, 238, 236, 15]]
-    else:
-        raise ValueError("invalid palette {}".format(palette))
-    
-    return _best(color16_mapping, [r, g, b])[3]
+def _rgb_to_16(r, g, b, palette):
+    r, g, b = map(int, (r, g, b))
+
+    return _best(_get_system_colors(palette), [r, g, b])[3]
+
+def _rgb_to_8(r, g, b, palette):
+    # 8 bit is simply the non-dark versions of the 16 system colors
+    r, g, b = map(int, (r, g, b))
+    return _best(_get_system_colors(palette)[:8], [r, g, b])[3]
 
 # convert the rgb value into an escape sequence
-def _pix_to_escape(r, g, b, is_truecolor):
-    if is_truecolor:
+def _pix_to_escape(r, g, b, color_type, palette):
+    if color_type == _color_types.truecolor:
         return '\x1b[48;2;{};{};{}m  '.format(r, g, b)
+    elif color_type == _color_types.color256:
+        return '\x1b[48;5;{}m  '.format(_rgb_to_256(r, g, b, palette))
+    elif color_type == _color_types.color16:
+        escape_code_id = _rgb_to_16(r, g, b, palette)
+        codepoint = None
+        if escape_code_id >= 8:
+            codepoint = '10' + str(escape_code_id - 8)
+        else:
+            codepoint = '4' + str(escape_code_id)
+        return '\x1b[{}m  '.format(codepoint)
+    elif color_type == _color_types.color8:
+        escape_code_id = _rgb_to_8(r, g, b, palette)
+        return '\x1b[4{}m  '.format(escape_code_id)
     else:
-        return '\x1b[48;5;{}m  '.format(_rgb_to_256(r, g, b))
+        raise ValueError("invalid color_type: {}".format(color_type))
 
 # convert the two row's colors to a escape sequence (unicode does two rows at a time)
-def _dual_pix_to_escape(r1, r2, g1, g2, b1, b2, is_truecolor):
-    if is_truecolor:
+def _dual_pix_to_escape(r1, r2, g1, g2, b1, b2, color_type, palette):
+    if color_type == _color_types.truecolor:
         return '\x1b[48;2;{};{};{}m\x1b[38;2;{};{};{}m▄'.format(r1, g1, b1, r2, g2, b2)
+    elif color_type == _color_types.color256:
+        return '\x1b[48;5;{}m\x1b[38;5;{}m▄'.format(_rgb_to_256(r1, g1, b1, palette), _rgb_to_256(r2, g2, b2, palette))
+    elif color_type == _color_types.color16:
+        bg_code_id = _rgb_to_16(r1, g1, b1, palette)
+        fg_code_id = _rgb_to_16(r2, g2, b2, palette)
+        bg_codepoint = None
+        fg_codepoint = None
+        if bg_code_id >= 8:
+            bg_codepoint = '10' + str(bg_codepoint - 8)
+        else:
+            bg_codepoint = '4' + str(bg_codepoint)
+        if fg_codepoint >= 8:
+            fg_codepoint = '9' + str(fg_codepoint - 8)
+        else:
+            fg_codepoint = '3' + str(fg_codepoint)
+        return '\x1b[{}m\x1b[{}m▄'.format(bg_codepoint, fg_codepoint)
+    elif color_type == _color_types.color8:
+        bg_code_id = _rgb_to_8(r1, g1, b1, palette)
+        fg_code_id = _rgb_to_8(r2, g2, b2, palette)
+        return '\x1b[4{}m\x1b[3{}m▄'.format(bg_code_id, fg_code_id)
     else:
-        return '\x1b[48;5;{}m\x1b[38;5;{}m▄'.format(_rgb_to_256(r1, g1, b1), _rgb_to_256(r2, g2, b2))
+        raise ValueError("invalid color_type: {}".format(color_type))
 
-def _toAnsi(img, oWidth, is_unicode=False, is_truecolor=False):
+def _toAnsi(img, oWidth, is_unicode, color_type, palette):
     destWidth = img.width
     destHeight = img.height
     scale = destWidth / oWidth
@@ -114,9 +143,9 @@ def _toAnsi(img, oWidth, is_unicode=False, is_truecolor=False):
             if is_unicode:
                 # the next row's pixel
                 rprime, gprime, bprime = map(str, img.getpixel((x, y+1)))
-                ansi_string += _dual_pix_to_escape(r, rprime, g, gprime, b, bprime, is_truecolor)
+                ansi_string += _dual_pix_to_escape(r, rprime, g, gprime, b, bprime, color_type, palette)
             else:
-                ansi_string += _pix_to_escape(r, g, b, is_truecolor)
+                ansi_string += _pix_to_escape(r, g, b, color_type, palette)
         # line ending, reset colours
         ansi_string += '\x1b[0m\n'
         if is_unicode:
